e850fea328d84f6638e3af2a55b6260387bcb1d3
Yugovtr <vtrhg69@hotmail.com>
2018-12-11 18:26:43 -0200

    Observer Chat GUI

--- src/observer/ChatWindow.java
+++ src/observer/ChatWindow.java
@@ -1,20 +1,31 @@
 package observer;
 
+import java.io.IOException;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author Yugo
  */
 public class ChatWindow extends javax.swing.JFrame implements Observer {
 
+    Cliente cliente; 
     /**
      * Creates new form ChatWindow
      */
     public ChatWindow() {
+        try {
+            this.cliente = new Cliente("10.62.0.60",7777);
+            cliente.addObserver(this);
+        } catch (Exception ex) {
+            System.out.println("Erro: " + ex.getMessage());
+        }
         initComponents();
+        
     }
-
+   
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -24,21 +35,61 @@ public class ChatWindow extends javax.swing.JFrame implements Observer {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea_Output = new javax.swing.JTextArea();
+        jTextField_Input = new javax.swing.JTextField();
+        jButton_Enviar = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jTextArea_Output.setColumns(20);
+        jTextArea_Output.setRows(5);
+        jScrollPane1.setViewportView(jTextArea_Output);
+
+        jButton_Enviar.setText("Enviar");
+        jButton_Enviar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton_EnviarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 380, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jTextField_Input)
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton_Enviar)))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 118, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTextField_Input, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton_Enviar))
+                .addContainerGap(130, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton_EnviarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_EnviarActionPerformed
+        String msg = this.jTextField_Input.getText(); 
+        try {
+            this.cliente.enviarMsg(msg);
+        } catch (IOException ex) {
+            Logger.getLogger(ChatWindow.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_jButton_EnviarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -81,6 +132,7 @@ public class ChatWindow extends javax.swing.JFrame implements Observer {
             if(o instanceof Cliente) {
                 Cliente server = (Cliente) o; 
                 String msg = server.getMsg(); 
+                this.jTextArea_Output.setText(this.jTextArea_Output.getText() + "\n" + msg); 
             }
         } catch (Exception e) {
             System.out.println("Erro: " + e.getMessage());
@@ -88,5 +140,9 @@ public class ChatWindow extends javax.swing.JFrame implements Observer {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton_Enviar;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea jTextArea_Output;
+    private javax.swing.JTextField jTextField_Input;
     // End of variables declaration//GEN-END:variables
 }
